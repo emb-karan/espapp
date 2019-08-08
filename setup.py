0ec4b4600850b482dccad0578ba6e06cd9d8c8b0
VIRUBN20190604 <emb-karan>
2019-08-08 17:50:29 +0530

    semifinal

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
-from setuptools import setup
+import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name='espapp',
+setuptools.setup(name='espapp_pkg',
       version='0.3',
       description='ESP applicaion',
       author='Karan',
       author_email='karan.rathore@viriminfotech.com',
       license='Virim',
+      url = "https://github.com/emb-karan/espapp",
+      packages=setuptools.find_packages(),
       install_requires=[
           'uuid',
           'python-crontab',
           'requests',
       ],
+      entry_points={  # Optional
+        'console_scripts': [
+            'espapp=espapp:main',
+        ],
+       },
       )
       
